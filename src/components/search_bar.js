7ab67a608ed2de9a802b3c66fe83498fea1cb619
Ryan Frazier <rfrazier@jacksonfurnind.com>
2018-03-22 21:44:06 -0400

    updated Input to handle changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,8 +8,36 @@ import React, { Component } from 'react';
 
 // ES6 Class  :  render is a "method"
 class SearchBar extends Component {
+    // ========================================================================
+        // Constructor gets called at ever Class Initiated before it's mounted. 
+        // Used for set up like initializing variables.
+        // If "super(props);" isn't put first... "this.props" will be undefined.    
+    // ========================================================================
+    constructor(props){
+        super(props);
+        // this.state is only in constructor. Elsewhere, it would be this.setState({})
+        this.state = { term: '' };
+    }
+    
     render() {
-        return <input />
+        // return <input onChange={(e) => console.log(e.target.value)} />
+        // Must put "".bind(this)"" to bind context of "this" in onInputChange Method
+        // basically to use "this."
+        return (
+            <div>
+                {/* When Value is set to 'value={this.state.term}' what's happening is it looks at the term state and set it to what that is. In this case, it's blank. As the user starts to type the "onChange" method rerenders each change and then what was typed is setting the state therefor making the value equal to what was typed.  "Called Controlled Components where you receive the value from state" */}
+                <input 
+                    value={this.state.term}
+                    onChange={this.onInputChange.bind(this)} 
+                />
+            </div> 
+        );
+    
+    }
+    
+    // Event handlers = Below is Methods
+    onInputChange(e){
+         this.setState({ term: e.target.value });
     }
 }
 
