3699b4762ee850c747864f8ba10f71cf8c9e1eee
Ryan Frazier <rfrazier@jacksonfurnind.com>
2018-04-03 16:07:55 -0400

    added functionality to searching

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,5 @@
 // Global Imports
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // { Component } is same as React.Component which comes after extend on ES6 Class
 
 // const SearchBar = () => {
@@ -8,37 +8,38 @@ import React, { Component } from 'react';
 
 // ES6 Class  :  render is a "method"
 class SearchBar extends Component {
-    // ========================================================================
-        // Constructor gets called at ever Class Initiated before it's mounted. 
-        // Used for set up like initializing variables.
-        // If "super(props);" isn't put first... "this.props" will be undefined.    
-    // ========================================================================
-    constructor(props){
-        super(props);
-        // this.state is only in constructor. Elsewhere, it would be this.setState({})
-        this.state = { term: '' };
-    }
-    
-    render() {
-        // return <input onChange={(e) => console.log(e.target.value)} />
-        // Must put "".bind(this)"" to bind context of "this" in the onInputChange Method
-        // basically to use "this."
-        return (
-            <div>
-                {/* When Value is set to 'value={this.state.term}' what's happening is it looks at the term state and set it to what that is. In this case, it's blank. As the user starts to type the "onChange" method rerenders each change and then what was typed is setting the state therefor making the value equal to what was typed.  "It's called (Controlled Components) where you receive the value from state" */}
-                <input 
-                    value={this.state.term}
-                    onChange={this.onInputChange.bind(this)} 
-                />
-            </div> 
-        );
-    
-    }
-    
-    // Event handlers = Below is Methods
-    onInputChange(e){
-         this.setState({ term: e.target.value });
-    }
+  // ========================================================================
+  // Constructor gets called at ever Class Initiated before it's mounted.
+  // Used for set up like initializing variables.
+  // If "super(props);" isn't put first... "this.props" will be undefined.
+  // ========================================================================
+  constructor(props) {
+    super(props);
+    // this.state is only in constructor. Elsewhere, it would be this.setState({})
+    this.state = { term: "" };
+  }
+
+  render() {
+    // return <input onChange={(e) => console.log(e.target.value)} />
+    // Must put "".bind(this)"" to bind context of "this" in the onInputChange Method
+    // basically to use "this."
+    return (
+      <div className="search-bar">
+        {/* When Value is set to 'value={this.state.term}' what's happening is it looks at the term state and set it to what that is. In this case, it's blank. As the user starts to type the "onChange" method rerenders each change and then what was typed is setting the state therefor making the value equal to what was typed.  "It's called (Controlled Components) where you receive the value from state" */}
+        <input
+          value={this.state.term}
+          //   onChange={this.onInputChange.bind(this)}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
+
+  // Event handlers = Below is Methods
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // Global Imports
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import _ from "lodash";
 
 // Local Imports
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = config.API_KEY;
 /*
 React has what's known as downwards data flow. Only the most parent component should be
 responsible for fetching data.
 */
-
 
 // Create new component. This component should produce some HTML.
 // const App = () => {
@@ -28,43 +28,53 @@ responsible for fetching data.
 // When using state we need to use the Class based component instead of Functional.
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        
-        this.state = { 
-            videos: [],
-            selectedVideo: null //Initial for what was "selected" being nothing
-        }
-        //Now once page loads constructor will immediately fire with a list of videos instantiated below
-        YTSearch({ key: API_KEY, term: 'golf' }, (videos) => {
-            this.setState({ 
-                videos: videos,
-                selectedVideo: videos[0]
-            })
-            // When key and property are same with ES6, you can simply put it once like above. 
-            // ES5 version would be this.setState({ videos: videos })
-        });
-    }
-    
-    render(){
+  constructor(props) {
+    super(props);
 
-        return(
-            <div>
-                <SearchBar />
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList 
-                    videos={this.state.videos}
-                    onVideoSelect={ (selectedVideo) => this.setState({ selectedVideo }) }/>
-                    {/*passing "prop" videos (the State of videos) to VideoList*/}
-                   {/* called "onVideoSelect" function which takes a video (selectedVideo) and defines App state (setState) with a video from VideoList*/}
-                   {/* 
-                       with ES6... arrow functions () => don't have to have (). So it could have been
-                       selectedVideo => this.setState()
-                    */}
-            </div>
-        );
-    }
+    this.state = {
+      videos: [],
+      selectedVideo: null //Initial for what was "selected" being nothing
+    };
+
+    //Now once page loads constructor will immediately fire with a list of videos instantiated below
+    this.videoSearch("golf");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      // When key and property are same with ES6, you can simply put it once like above.
+      // ES5 version would be this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    // created a function which we passed to .debounce(lodash function). Debounce takes inner function and returns new function which can be called only ever 300 milliseconds
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 400);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+        {/*passing "prop" videos (the State of videos) to VideoList*/}
+        {/* called "onVideoSelect" function which takes a video (selectedVideo) and defines App state (setState) with a video from VideoList*/}
+        {/* 
+            with ES6... arrow functions () => don't have to have (). So it could have been
+            selectedVideo => this.setState()
+        */}
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put on the page (in the DOM).
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
